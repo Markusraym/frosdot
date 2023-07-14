4caec71e5f534e4692b8c972e63e144ad6852736
Alexander Murphy <alexander.murphy@orcro.co.uk>
2023-07-14 10:29:33 +0100

    Renamed clauses, removed old files, simple text to docx pythong script.

---
+++ CODE/markdownToWord.py
@@ -0,0 +1,19 @@
+# Requires the permissively licensed python-docx, available on PyPI (https://pypi.org/project/python-docx/)
+
+from docx import Document
+import argparse
+import re
+
+parser = argparse.ArgumentParser()
+parser.add_argument("fileName", help="Convert this file")
+args = parser.parse_args()
+
+document = Document()
+
+# Title
+title = re.sub(".txt$", "", args.fileName)
+document.add_heading(title)
+myfile = open(args.fileName).read()
+p = document.add_paragraph(myfile)
+
+document.save("output.docx")
