c9eb2ea21bb5a5daa59de54168c4fb8e310555a6
Alexander Murphy <alexander.murphy@orcro.co.uk>
2023-07-17 10:19:06 +0100

    Testing django functionality.

--- CODE/frosdotUI/mdToDocx/models.py
+++ CODE/frosdotUI/mdToDocx/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Query(models.Model):
+    query_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+    def __str__(self):
+        return self.query_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Response(models.Model):
+    question = models.ForeignKey(Query, on_delete=models.CASCADE)
+    response_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
