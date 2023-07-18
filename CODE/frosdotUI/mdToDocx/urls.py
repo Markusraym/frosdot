738c65e4981d23a6167b1a2b8a07f039b7077843
Alexander Murphy <alexander.murphy@orcro.co.uk>
2023-07-18 16:46:23 +0100

    Adding some depth and templates.

--- CODE/frosdotUI/mdToDocx/urls.py
+++ CODE/frosdotUI/mdToDocx/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = "queries"
 urlpatterns = [
+        # e.g. /mdToDocx
         path("", views.index, name="index"),
+        # e.g. /mdToDocx/1/
+        path("<int:query_id>/", views.detail, name="detail"),
+        # e.g. /mdToDocx/1/results/
+        path("<int:query_id>/results/", views.results, name="results"),
+        # e.g. /mdToDocx/1/mark/
+        path("<int:query_id>/mark/", views.mark, name="marks"),
         ]
--- CODE/frosdotUI/mdToDocx/views.py
+++ CODE/frosdotUI/mdToDocx/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
+# from django.http import Http404
+
+from .models import Query
 
 def index(request):
-    return HttpResponse("Hello world from Django")
+    latest_query_list = Query.objects.order_by("-pub_date")[:5]
+    context = { "latest_query_list": latest_query_list, }
+    return render(request, "mdToDocx/index.html", context)
+
+def detail(request, query_id):
+    query = get_object_or_404(Query, pk=query_id)
+    return render(request, "mdToDocx/detail.html", {"query": query})
+
+def results(request, query_id):
+    response = "Viewing results of query %s."
+    return HttpResponse(response % query_id)
+
+def mark(request, query_id):
+    return HttpResponse("Mark question %s." % query_id)
+
